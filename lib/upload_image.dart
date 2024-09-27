588f5ed77771b6a061b6ffb01d147ad64b99989f
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-09-27 12:13:43 +0500

    added getImage function

--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -5,48 +5,60 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
-class upload_image extends StatefulWidget {
-  const upload_image({super.key});
+class UploadImage extends StatefulWidget {
+  const UploadImage({super.key});
 
   @override
-  State<upload_image> createState() => _upload_imageState();
+  State<UploadImage> createState() => _UploadImageState();
 }
 
-class _upload_imageState extends State<upload_image> {
+class _UploadImageState extends State<UploadImage> {
+  File? image;
+  final ImagePicker _picker = ImagePicker();
+  bool showSpinner = false;
 
-File? image;
-final _picker = ImagePicker();
-bool showSpinner = false;
+  Future<void> getImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
 
-Future getImage()async{
-  final pickedFile = await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
+    if (pickedFile != null) {
+      setState(() {
+        image = File(pickedFile.path);
+      });
+    } else {
+      print("No image selected");
+    }
+  }
+Future<void>uploadImage()async{
+    
 }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign Up API'),
+        title: const Text('Image Picker Example'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.photo),
+            onPressed: getImage,
+          ),
+        ],
       ),
-      body:  Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center
-        ,
+      body: Center(
+        child: image == null
+            ? const Text('Pick an Image')
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              child: image == null? Center(child: Text('Pick Image'),)
+            Image.file(
+              image!,
+              height: 100,
+              width: 200,
+              fit: BoxFit.cover,
             ),
-            Container(
-              child: Center(
-                child: Image.file(File(
-                  image!.path).absolute,
-                    height: 100,
-                  width: 200,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            )
+            const SizedBox(height: 20),
           ],
-        )
+        ),
+      ),
     );
   }
 }
