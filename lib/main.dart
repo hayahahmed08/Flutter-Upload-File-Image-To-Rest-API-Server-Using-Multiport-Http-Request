6408b001473f962fa18185bd4a1e5b568e67d83c
hayahahmed08 <hayahahmeduser2@gmail.com>
2024-09-27 21:54:40 +0500

    updated uploadImage func

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:image_upload/upload_image.dart.dart';
+import 'package:image_upload/upload_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const SignUpScreen(),
+      home: const UploadImage(),
     );
   }
 }
--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class UploadImage extends StatefulWidget {
   const UploadImage({super.key});
@@ -28,35 +29,44 @@ class _UploadImageState extends State<UploadImage> {
       print("No image selected");
     }
   }
-Future<void>uploadImage()async{
-    
+Future<void>uploadImage()async{  //this function is created so that when the file is being uploaded a temporarily loading progress bar is shown
+setState(() {
+  showSpinner = true;
+});
+//now we gotta read image as a stream
+var stream = new http.ByteStream(image!.openRead());
+stream.cast();
+var length = await image!.length();
 }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Image Picker Example'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.photo),
-            onPressed: getImage,
-          ),
-        ],
-      ),
-      body: Center(
-        child: image == null
-            ? const Text('Pick an Image')
-            : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.file(
-              image!,
-              height: 100,
-              width: 200,
-              fit: BoxFit.cover,
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Picker Example'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.photo),
+              onPressed: getImage,
             ),
-            const SizedBox(height: 20),
           ],
+        ),
+        body: Center(
+          child: image == null
+              ? const Text('Pick an Image')
+              : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.file(
+                image!,
+                height: 100,
+                width: 200,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
